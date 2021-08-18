3d5f82936a43118d67adef3f7440d1b3ec2c757f
binhk1004 <binhk1004@naver.com>
2021-08-18 21:18:29 +0900

    [Styled] Apply style

--- src/Bio.js
+++ src/Bio.js
@@ -1,16 +1,21 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './Bio.css';
+
 
 function Bio() {
     return (
       <div className="Bio">
         <header>
           <div>
-            <img src="images/main_profile.jpeg" alt="증명사진"></img>
+            <div class="hello">
+              <h1>안녕하세요!</h1>
+              <h2>저는 김빈현 이라고 합니다!</h2>
+            </div>
           </div>
         </header>
 
       </div>
-    );
+    )
   }
   
   export default Bio;
