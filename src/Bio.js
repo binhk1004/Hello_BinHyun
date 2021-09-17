fa1f99343c9104a6e3b7df9bb9092d255e68f0db
binhk1004 <binhk1004@naver.com>
2021-09-17 20:19:19 +0900

    [Components] Added Components 'AboutMe.js'

--- src/Bio.js
+++ src/Bio.js
@@ -1,20 +1,45 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './Bio.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Nav } from "react-bootstrap";
+import { Container } from "react-bootstrap";
+import { Col } from "react-bootstrap";
+import {Image} from "react-bootstrap"
 
+import "./Bio.css";
 
 function Bio() {
-    return (
-      <div className="Bio">
-        <header>
-            <div className="hello">
-              <h1>안녕하세요!</h1>
-              <h2>저는 김빈현 이라고 합니다!</h2>
-          </div>
-        </header>
+  return (
+    <div className="Bio">
+      <nav className="nav">
+        <Nav variant="pills" defaultActiveKey="/home">
+          <Nav.Item>
+            <Nav.Link href="/home">Home</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link eventKey="link-1">Bio</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link eventKey="link-2">Carrer</Nav.Link>
+          </Nav.Item>
+        </Nav>
+      </nav>
+      <header className="hello">
+        <Container>
+          <Col xs={15} md={20}>
+            <Image className="photo" src="images/main_profile.jpeg" roundedCircle />
+          </Col>
+        </Container>
+        <br></br>
+        <div>
+          <h1>김빈현의 포트폴리오</h1>
+        </div>
+         <br></br>
+        <div>
+          반갑습니다!<br></br>
+          개발이 좋아서 여기까지 오게 된 김빈현 이라고 합니다!
+        </div>
+      </header>
+    </div>
+  );
+}
 
-      </div>
-    )
-  }
-  
-  export default Bio;
-  
+export default Bio;
