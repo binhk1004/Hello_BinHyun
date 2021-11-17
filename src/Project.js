3a32f46828e173a1267c240c2b480d39c65374bd
binhk1004 <binhk1004@naver.com>
2021-11-17 15:19:02 +0900

    [Components] Modified 'Project.js'

--- src/Project.js
+++ src/Project.js
@@ -1,4 +1,4 @@
-import { Card, Button } from "react-bootstrap"
+import { Card } from "react-bootstrap"
 import "./Project.css";
 
 
@@ -9,13 +9,27 @@ const Project = () => {
                 <h1>Project</h1>
             </div>
             <div>
-                <Card border="dark" className="card-list" style={{ width: '60rem', height: '60rem' }}>
+                <Card border="dark" className="card-list" style={{ width: '55rem', height: '55rem' }}>
+                    <div className="card-title">
+                        <h3>나의 맛집을 찾아보자!</h3>
+                    </div>
                     <Card.Body className="test">
                         <Card.Img variant="top" src="images/java.svg" />
-                        <Card.Text>
-                        Some quick example text to build on the card title and make up the bulk of
-                        the card's content.
-                        </Card.Text>
+                        <div className="card-text">
+                            <Card.Text>
+                            국비지원 '스마트웹자바' 과정을 수강 한 뒤 만들어본 Spring 게시판 프로젝트 입니다.
+                            Kakao API를 이용하여 지역명과 음식명을 검색 할 수 있습니다.
+                            게시판의 응용버전이라고 볼 수 있는 프로젝트로써,
+                            회원가입 기능 및 게시물 작성, 수정, 삭제 기능을 포함하고 있습니다.
+                            또한 관리자 계정으로 로그인시 전체 게시물을 확인 할 수 있으며, <br></br>
+                            수정, 삭제가 가능합니다. 
+                            
+                            </Card.Text>
+                            <Card.Text>
+                            Some quick example text to build on the card title and make up the bulk of
+                            the card's content.
+                            </Card.Text>
+                        </div>
                     </Card.Body>
                     {/* <Button className="button" href="#">Card Link</Button> */}
                 </Card>
